12c01cb6dc47db17af292dd64c9ef9bd20637b46
Frank Benkstein <frank@benkstein.net>
2018-10-21 15:06:58 +0200

    always use re2c to generate lexers

---
+++ cmake/RE2CTarget.cmake
@@ -0,0 +1,39 @@
+#=============================================================================
+# Copyright (c) 2015, julp
+#
+# Distributed under the OSI-approved BSD License
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#=============================================================================
+
+include(CMakeParseArguments)
+
+function(RE2C_TARGET)
+    cmake_parse_arguments(PARSED_ARGS "" "NAME;INPUT;OUTPUT" "OPTIONS;DEPENDS" ${ARGN})
+
+    if(NOT PARSED_ARGS_OUTPUT)
+        message(FATAL_ERROR "RE2C_TARGET expect an output filename")
+    endif(NOT PARSED_ARGS_OUTPUT)
+    if(NOT PARSED_ARGS_INPUT)
+        message(FATAL_ERROR "RE2C_TARGET expect an input filename")
+    endif(NOT PARSED_ARGS_INPUT)
+    if(NOT PARSED_ARGS_NAME)
+        message(FATAL_ERROR "RE2C_TARGET expect a target name")
+    endif(NOT PARSED_ARGS_NAME)
+    # TODO:
+    # - get_filename_component(PARSED_ARGS_INPUT ${PARSED_ARGS_INPUT} ABSOLUTE)
+    # - get_filename_component(PARSED_ARGS_OUTPUT ${PARSED_ARGS_OUTPUT} ABSOLUTE)
+    # ?
+    add_custom_command(
+        OUTPUT ${PARSED_ARGS_OUTPUT}
+        COMMAND RE2C::RE2C ${PARSED_ARGS_OPTIONS} -o ${PARSED_ARGS_OUTPUT} ${PARSED_ARGS_INPUT}
+        DEPENDS ${PARSED_ARGS_INPUT} ${PARSED_ARGS_DEPENDS}
+        COMMENT "[RE2C][${PARSED_ARGS_NAME}] Building lexer with re2c"
+    )
+    add_custom_target(
+        ${PARSED_ARGS_NAME}
+        SOURCES ${PARSED_ARGS_INPUT}
+        DEPENDS ${PARSED_ARGS_OUTPUT}
+    )
+endfunction(RE2C_TARGET)
